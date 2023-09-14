04c522e0e2ef27aff33619b79642f6120189bd1c
Espoir <386707112@qq.com>
2023-09-14 17:20:34 +0800

    创建项目
    创建项目

---
+++ app/src/main/java/com/espoir/glidedsl/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.espoir.glidedsl
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
